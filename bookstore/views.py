bd238c2db2c6a9f53e0f5a17073e6111906340f2
Zuzanna Zajkiewicz <machefi1@gmail.com>
2021-06-10 17:03:17 +0200

    Ass basic styling

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+books = [
+    {
+        'author': 'Terry Pratchett',
+        'title': 'Guards! Guards!',
+        'description': 'Lorem ipsum simit dolor...',
+        'year_published': '2012',
+        'status': 'available'
+    },
+    {
+        'author': 'Paolo Bacigalupi',
+        'title': 'The Windup Girl',
+        'description': 'Lorem ipsum simit dolor...',
+        'year_published': '2010',
+        'status': 'available'
+    },
+    {
+        'author': 'Andrew Davidson',
+        'title': 'The Gargoyle',
+        'description': 'Lorem ipsum simit dolor...',
+        'year_published': '2009',
+        'status': 'available'
+    },
+    {
+        'author': 'Hans Rosling',
+        'title': 'Factfulness',
+        'description': 'Lorem ipsum simit dolor...',
+        'year_published': '2018',
+        'status': 'available'
+    },
+
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Library main page</h1>')
+    context = {
+        'books': books
+    }
+    return render(request, 'bookstore/home.html', context)
