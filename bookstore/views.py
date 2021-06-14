6e183c3ed19dd47bc92eab453b86851eb6b754f0
Zuzanna Zajkiewicz <machefi1@gmail.com>
2021-06-14 19:48:31 +0200

    Add book model and basic user profile

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,41 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Book
 
-books = [
-    {
-        'author': 'Terry Pratchett',
-        'title': 'Guards! Guards!',
-        'description': 'Lorem ipsum simit dolor...',
-        'year_published': '2012',
-        'status': 'available'
-    },
-    {
-        'author': 'Paolo Bacigalupi',
-        'title': 'The Windup Girl',
-        'description': 'Lorem ipsum simit dolor...',
-        'year_published': '2010',
-        'status': 'available'
-    },
-    {
-        'author': 'Andrew Davidson',
-        'title': 'The Gargoyle',
-        'description': 'Lorem ipsum simit dolor...',
-        'year_published': '2009',
-        'status': 'available'
-    },
-    {
-        'author': 'Hans Rosling',
-        'title': 'Factfulness',
-        'description': 'Lorem ipsum simit dolor...',
-        'year_published': '2018',
-        'status': 'available'
-    },
+# books = [
+#     {
+#         'author': 'Terry Pratchett',
+#         'title': 'Guards! Guards!',
+#         'description': 'Lorem ipsum simit dolor...',
+#         'year_published': '2012',
+#         'status': 'available'
+#     },
+#     {
+#         'author': 'Paolo Bacigalupi',
+#         'title': 'The Windup Girl',
+#         'description': 'Lorem ipsum simit dolor...',
+#         'year_published': '2010',
+#         'status': 'available'
+#     },
+#     {
+#         'author': 'Andrew Davidson',
+#         'title': 'The Gargoyle',
+#         'description': 'Lorem ipsum simit dolor...',
+#         'year_published': '2009',
+#         'status': 'available'
+#     },
+#     {
+#         'author': 'Hans Rosling',
+#         'title': 'Factfulness',
+#         'description': 'Lorem ipsum simit dolor...',
+#         'year_published': '2018',
+#         'status': 'available'
+#     },
 
-]
+# ]
 
 
 def home(request):
     context = {
-        'books': books
+        'books': Book.objects.all()
     }
     return render(request, 'bookstore/home.html', context)
