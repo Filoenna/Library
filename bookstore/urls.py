a869de05987b2564749a4986c669884c5af3e328
Zuzanna Zajkiewicz <machefi1@gmail.com>
2021-06-22 11:40:18 +0200

    Add: create book, rent a book, return a book

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,6 +1,13 @@
+from bookstore.models import Book
 from django.urls import path
+from .views import BookListView, BookDetailView, BookCreateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='library-home'),
+    path('', BookListView.as_view(), name='library-home'),
+    path('book/<int:new_pk>/rent', views.rent, name='rent-book'),
+    path('book/<int:new_pk>/return', views.breturn, name='return-book'),
+    path('book/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('book/new/', BookCreateView.as_view(), name='book-create'),
+    
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
 from .models import Book
 
 def home(request):
@@ -6,3 +10,39 @@ def home(request):
         'books': Book.objects.all()
     }
     return render(request, 'bookstore/home.html', context)
+
+@login_required
+def rent(request, new_pk):
+    book = Book.objects.get(pk=new_pk)
+    book.status = 'rented'
+    book.lent_to = request.user
+    book.save()
+    return redirect('library-home')
+
+def breturn(request, new_pk):
+    book = Book.objects.get(pk=new_pk)
+    book.status = 'available'
+    book.lent_to = None
+    book.save()
+    return redirect('library-home')
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'bookstore/home.html'
+    context_object_name = 'books'
+    ordering = ['author']
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = 'book'
+
+class BookCreateView(LoginRequiredMixin, CreateView):
+    model = Book
+    context_object_name = 'book'
+    fields = ['author','title', 'description']
+
+
+
+
+
+
